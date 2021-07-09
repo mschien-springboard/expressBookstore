dc14c2c3ecac2b4b971573cf17e831847f31d326
Michael Schienbein <moomba0@gmail.com>
2021-07-09 18:07:03 +0700

    initial commit

---
+++ data_test.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS books_test;
+CREATE DATABASE books_test;
+
+\c books_test;
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
